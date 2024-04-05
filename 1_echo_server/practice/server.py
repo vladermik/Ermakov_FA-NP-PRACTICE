31c00f8949909bd3c93b316dbaad9a2a3efd0904
vladermik <vladermik@gmail.com~>
2024-04-05 15:02:40 +0300

    3+7

--- 1_echo_server/practice/server.py
+++ 1_echo_server/practice/server.py
@@ -5,7 +5,7 @@ from functions import *
 sock = socket.socket()
 log(1, "Server is activated")
 port = str(input("Введите номер порта: "))
-port = port if len(port) > 3 else 9090
+port = int(port) if len(port) > 3 else 9090
 while True:
 	try:
 		sock.bind(("", port))
@@ -14,23 +14,25 @@ while True:
 	except OSError:
 		print(f"port is busy. trying {port+1}")
 		port += 1
-
-sock.listen(0)
-log(1, f"listening on port {port}")
-conn, addr = sock.accept()
-log(1, f"got connection from {addr[0]}:{addr[1]}")
-
-msg = ''
-
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	log(1, f"got message from {addr[0]}:{addr[1]}")
-	msg += data.decode()
-	conn.send(data)
-	log(1, f"send message from {addr[0]}:{addr[1]}")
-	print(msg)
+	try:
+		sock.listen(0)
+		log(1, f"listening on port {port}")
+		conn, addr = sock.accept()
+		log(1, f"got connection from {addr[0]}:{addr[1]}")
 
+		msg = ''
 
-#conn.close()
+		while True:
+			data = conn.recv(1024)
+			if not data:
+				break
+			log(1, f"got message from {addr[0]}:{addr[1]}")
+			msg += data.decode()
+			conn.send(data)
+			log(1, f"send message to {addr[0]}:{addr[1]}")
+			print(msg)
+	except ConnectionResetError:
+		continue
+
+	#conn.close()
